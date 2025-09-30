74d421cf31d27ba774fbf4ff86b8e1068a5b74a7
Tbzz83 <tbzz83@gmail.com>
2025-09-30 08:33:54 -0400

    moving typespeed and renaming

---
+++ internal/app/typespeed/parse.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Config struct {
+	Prompts        []Prompt `yaml:"prompts"`
+	ActivePromptID int
+	SeenIDs        map[int]int
+}
+
+type Prompt struct {
+	ID         int    `yaml:"id"`
+	Text       string `yaml:"text"`
+	Difficulty string `yaml:"difficulty"`
+}
+
+func parseYAML(filePath string) (*Config, error) {
+	path := "library.yaml"
+	if filePath != "" {
+		path = filePath
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+
+	if err = yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+
+}
